162d7b6f18d50c3bade4e26371b40f4dbd806b45
tokuhirom <tokuhirom@gmail.com>
2013-03-14 17:10:42 +0900

    initial import

---
+++ lib/Amon2/Plugin/ShareDir.pm
@@ -0,0 +1,41 @@
+package Amon2::Plugin::ShareDir;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Amon2::Plugin::ShareDir - ...
+
+=head1 SYNOPSIS
+
+  use Amon2::Plugin::ShareDir;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::ShareDir is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
