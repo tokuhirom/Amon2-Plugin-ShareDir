2077d9efd2df4d949637c6a33f8da31f0b37c39a
tokuhirom <tokuhirom@gmail.com>
2013-04-03 10:52:01 +0900

    Correct semantic versioning

--- lib/Amon2/Plugin/ShareDir.pm
+++ lib/Amon2/Plugin/ShareDir.pm
@@ -2,7 +2,7 @@ package Amon2::Plugin::ShareDir;
 use strict;
 use warnings;
 use 5.008005;
-our $VERSION = '0.0.3';
+use version; our $VERSION = version->declare("v0.0.4");
 
 use File::Spec;
 use File::ShareDir;
